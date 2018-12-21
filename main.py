1ff32a8e8bfbaa69c48803a9a624a352c3fff0b0
lylin17 <40259937+lylin17@users.noreply.github.com>
2018-12-21 18:19:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def clear():
 # No caching at all for API endpoints.
 @app.after_request
 def add_header(response):
-    # response.cache_control.no_store = True
+    response.cache_control.no_store = True
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
