5e53d4dfc5565ce1b28709ab952444d4527ade9c
lylin17 <40259937+lylin17@users.noreply.github.com>
2018-12-21 18:46:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def add_header(response):
     response.cache_control.no_store = True
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '-1'
+    response.headers['Expires'] = '0'
     return response
 
 if __name__ == "__main__":
