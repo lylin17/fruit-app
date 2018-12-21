81de8d5965fd78d1bad13fd6ab508607d9451516
lylin17 <40259937+lylin17@users.noreply.github.com>
2018-12-21 17:43:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import os
 import h5py
 from google.cloud import storage
 #import cloudstorage
-from urllib import urlopen
+import urllib
 
 def load_keras_model():
     """Load in the pre-trained model"""
@@ -26,7 +26,7 @@ def load_keras_model():
     
     global model
     #h5str = copy.copy(blob.download_as_string())
-    model2 = load_model(urlopen('http://storage.googleapis.com/staging.app-project-226107.appspot.com/resnet.h5'))
+    model2 = load_model(urllib.urlopen('http://storage.googleapis.com/staging.app-project-226107.appspot.com/resnet.h5'))
     #del h5str
             
     global graph
